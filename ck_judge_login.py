ce30c0b4723cdb63d06c9fbdf722f9e8f840312e
顧泓 <140719255+monesijd@users.noreply.github.com>
2024-09-08 11:42:59 +0800

    新增環境變數

--- ck_judge_login.py
+++ ck_judge_login.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
 import time
 import random
 
@@ -9,11 +10,11 @@ def scrap():
     driver.get('https://ckj.imslab.org/#/signin')
     time.sleep(3)
     username = driver.find_element(By.ID, "input-74")
-    username.send_keys("kou950804")
+    username.send_keys(os.environ['account'])
 
     username = driver.find_element(By.ID, "input-78")
 
-    username.send_keys("kou950804")
+    username.send_keys(os.environ['password'])
     button = driver.find_element(By.XPATH, "//button[contains(@class, 'primary-button') and @type='button' and contains(., 'Sign in')]")
     button.click()
 
@@ -27,10 +28,10 @@ def scrap():
 
 pre = 0
 while(1):
-    time.sleep(random.uniform(5, 10))
+    time.sleep(random.uniform(0, 1))
     now = scrap()
     if(pre != now):
-        print("change")
+        print("有新的 CK judge 加分作業")
         pre = now
     else:
         print("not change")
